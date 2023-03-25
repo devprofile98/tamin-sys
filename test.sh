fb9d5cce18115677d99acb2f845d0e48a3b59f16
Debian Live user <user@debian-BULLSEYE-live-builder-AMD64>
2023-03-25 18:11:04 +0000

    05/01/02

--- test.sh
+++ test.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
 SNAKE="\u25A0"
+S_HEAD="\u25A1"
 
 echo "Creating snake world"
-for i in {1..3}
+
+myarr=($SNAKE $SNAKE "\u25A1" "\n"
+	$S_HEAD $S_HEAD $S_HEAD "\n"
+	$S_HEAD $S_HEAD  $S_HEAD "\n"
+	)
+myarr2=($SNAKE $S_HEAD $S_HEAD "\n"
+	$S_HEAD $S_HEAD $S_HEAD "\n"	
+	$S_HEAD $S_HEAD $S_HEAD "\n"		
+)
+
+clear 
+
+for i in {0..9}
 do
-	printf .
-	sleep 0.5
-	printf "\c"
-	sleep 0.5
+	printf "%b " $SNAKE
+	if [ $(expr $i % 3) -eq 0 ]
+	then
+		printf "\n"
+	fi	
+clear
+
+for i in {0..9}
+do
+     printf "%b " $S_HEAD
+     # read -p "Waiting for your command cheif" cmd
+     if [ $(expr $i % 3 ) -eq 0 ]
+     then
+	     printf "\n"
+     fi
+     
 done
 
 
-printf "%b\n" $SNAKE
