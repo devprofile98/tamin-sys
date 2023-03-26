14cfb36e21f0b271eb1c577446c81776906bf4aa
Debian Live user <user@debian-BULLSEYE-live-builder-AMD64>
2023-03-26 19:13:18 +0000

    06/01/02-17:11

--- game.sh
+++ game.sh
@@ -2,8 +2,12 @@
 
 SNAKE="\u25A0"
 S_HEAD="\u25A1"
+ROWS=$(tput lines)
+COLS=$(tput cols)
 
-echo "Creating snake world"
+
+echo "Creating snake world with $COLS column and $ROWS rows"
+read -p "press any key to continue"
 
 myarr=($SNAKE $SNAKE "\u25A1" "\n"
 	$S_HEAD $S_HEAD $S_HEAD "\n"
@@ -18,16 +22,18 @@ clear
 
 for i in {0..9}
 do
-	printf "%b " $SNAKE
+	printf "%b" $SNAKE
 	if [ $(expr $i % 3) -eq 0 ]
 	then
 		printf "\n"
-	fi	
+	fi
+done
+sleep 0.5
 clear
 
 for i in {0..9}
 do
-     printf "%b " $S_HEAD
+     printf "%b" $S_HEAD
      # read -p "Waiting for your command cheif" cmd
      if [ $(expr $i % 3 ) -eq 0 ]
      then
@@ -36,4 +42,9 @@ do
      
 done
 
+sleep 0.5
+tput cup 12 12
+printf "%b" $S_HEAD
+sleep 2
+exit 0
 
